58060aa846a9b3a064f41a60bf761f74f94cfffe
Alexandr Racov <rakov.eres@gmail.com>
2024-08-23 19:36:47 +0300

    fixed logic by Mentor comments & maked some memory optimization

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,33 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func chessBoard(s int) string {
+	str := strings.Builder{}
+	// проходим по столбцам
+	for i := 0; i < s; i++ {
+		// проходим по строке
+		for j := 0; j < s; j++ {
+			// записываем в строку соотвествующий символ
+			if j%2 != 0 {
+				str.WriteString("#")
+			}
+			str.WriteString(" ")
+		}
+		// переходим на новую строку и если она четная, то начинаем ее с пробела
+		str.WriteString("\n")
+		if i%2 == 0 {
+			str.WriteString(" ")
+		}
+	}
+	return str.String()
+}
 
 func main() {
+
 	// Считываем целое число от пользователя - размер стороны доски
+	fmt.Println("Введи целое число от 1 до 200:")
 	var size int
 	_, err := fmt.Scanf("%d", &size)
 	if err != nil {
 		fmt.Printf("Input error: %s\n", err)
 		return
 	}
-
-	str := ""
-	// проходим по столбцам
-	for i := 0; i < size; i++ {
-		// проходим по строке
-		for j := 0; j < size; j++ {
-			// записываем в строку соотвествующий символ
-			if j%2 != 0 {
-				str += "#"
-			}
-			str += " "
-		}
-		// переходим на новую строку и если она четная, то начинаем ее с пробела
-		str += "\n"
-		if i%2 == 0 {
-			str += " "
-		}
-	}
-
-	fmt.Println(str)
+	fmt.Println(chessBoard(size))
 }
