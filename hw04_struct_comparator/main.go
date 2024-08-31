4ca11b747ee8fd7cc940820788ce9041673eb085
Alexandr Racov <rakov.eres@gmail.com>
2024-08-31 13:36:52 +0300

    HW04 is completed

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -1,5 +1,121 @@
 package main
 
+import "fmt"
+
+type Book struct {
+	id     int
+	title  string
+	author string
+	year   int
+	size   int
+	rate   float32
+}
+
+// enum компараторов
+
+type Comparator int
+
+const (
+	CompareByYear Comparator = iota
+	CompareBySize
+	CompareByRate
+)
+
+// BookComparator структура, наверное, не лучшее решение,
+// лучше реализовать при помощи интерфесов, но мы пока их не проходили.
+type BookComparator struct {
+	comparator Comparator
+}
+
+// Compare позволяет сравнивать две структуры Book по заданному
+// полю в зависимости от выбранного компаратора (сравнителя) в
+// конструкторе структуры BookComparator.
+func (bc BookComparator) Compare(b1, b2 Book) bool {
+	switch bc.comparator {
+	case CompareByYear:
+		return b1.year > b2.year
+	case CompareBySize:
+		return b1.size > b2.size
+	case CompareByRate:
+		return b1.rate > b2.rate
+	default:
+		return false
+	}
+}
+
+// геттеры для полей Book:
+
+func (b *Book) GetID() int {
+	return b.id
+}
+
+func (b *Book) GetTitle() string {
+	return b.title
+}
+
+func (b *Book) GetAuthor() string {
+	return b.author
+}
+
+func (b *Book) GetYear() int {
+	return b.year
+}
+
+func (b *Book) GetSize() int {
+	return b.size
+}
+
+func (b *Book) GetRate() float32 {
+	return b.rate
+}
+
+// сеттеры для полей Book:
+
+func (b *Book) SetID(id int) {
+	b.id = id
+}
+
+func (b *Book) SetTitle(title string) {
+	b.title = title
+}
+
+func (b *Book) SetAuthor(author string) {
+	b.author = author
+}
+
+func (b *Book) SetYear(year int) {
+	b.year = year
+}
+
+func (b *Book) SetSize(size int) {
+	b.size = size
+}
+
+func (b *Book) SetRate(rate float32) {
+	b.rate = rate
+}
+
 func main() {
-	// Place your code here.
+	AnnaKarenina := Book{
+		id:     1,
+		title:  "Анна Каренина",
+		author: "Лев Толстой",
+		year:   1878,
+		size:   1124,
+		rate:   4.75,
+	}
+
+	MartinIden := Book{
+		id:     2,
+		title:  "Мартин Иден",
+		author: "Джек Лондон",
+		year:   1909,
+		size:   448,
+		rate:   4.60,
+	}
+
+	bookComparator := BookComparator{comparator: CompareByRate}
+	fmt.Println(bookComparator.Compare(AnnaKarenina, MartinIden))
+
+	fmt.Println(BookComparator{comparator: CompareByYear}.Compare(AnnaKarenina, MartinIden))
 }
