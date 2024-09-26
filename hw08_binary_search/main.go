428e23ba1d7fc97c3f6c390e13cc37137d86026f
Alexandr Racov <rakov.eres@gmail.com>
2024-09-26 09:44:50 +0300

    HW08 is completed

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
-	// Place your code here.
+import "sort"
+
+func main() {}
+
+func BinarySearch(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	sort.Ints(nums)
+
+	low, high := 0, len(nums)-1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		switch {
+		case nums[mid] == target:
+			return mid
+		case nums[mid] > target:
+			high = mid - 1
+		default:
+			low = mid + 1
+		}
+	}
+
+	return -1
 }
